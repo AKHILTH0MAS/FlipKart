ec1c667ccbb903e9cc9f960753e7cf0fbf01c7cb
AKHILTH0MAS <akhilpthomas466@gmail.com>
2024-08-06 19:31:28 +0530

    push kardee

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ import {
 let products = [];
 let header;
 let relevanceProducts = [];
-let   filterContent = [];
+let filterContent = [];
 let filteredArray = [];
 
 function sortByColorReset() {
@@ -62,26 +62,48 @@ function addBrandsOnCategory() {
         filterCard();
         filterProducts();
       } else {
-        filterContent.splice(
-          filterContent.indexOf({ type: "brand", value: inputItem.value }),
-          1
+        let index = filterContent.findIndex(
+          (item) => item.type == "brand" && item.value == inputItem.value
         );
+        filterContent.splice(index, 1);
         filterCard();
-        filterProducts(); 
+        filterProducts();
       }
     });
   });
+}
+function clearPriceRange() {
+  let selectMin = document.getElementById("SelectMin");
+  let selectMax = document.getElementById("SelectMax");
+  selectMin.value = 0;
+  selectMax.value = 30001;
+  selectMax.dispatchEvent(new Event("click"));
+  selectMin.dispatchEvent(new Event("click"));
 }
 function filterCard() {
   let filterContainer = document.querySelector(".filter-content");
   filterContainer.innerHTML = "";
   filterContent.forEach((element) => {
-    let filterCardItem = ` 
+    if (element.type == "priceRange") {
+      console.log(element);
+      let filterCardItem = ` 
+    <div class="filtercard">
+        <span class="closeButton"> ✕ </span>
+        <span class="filterCardSpan">${
+          element.minValue == 0 ? "Min" : `₹${element.minValue}`
+        }-${
+        element.maxValue == 30001 ? "₹30000+" : `₹${element.maxValue}`
+      }</span>  
+    </div>`;
+      filterContainer.insertAdjacentHTML("afterbegin", filterCardItem);
+    } else {
+      let filterCardItem = ` 
     <div class="filtercard">
         <span class="closeButton"> ✕ </span>
         <span class="filterCardSpan">${element.value}</span>
     </div>`;
-    filterContainer.insertAdjacentHTML("afterbegin", filterCardItem);
+      filterContainer.insertAdjacentHTML("afterbegin", filterCardItem);
+    }
   });
 
   let closebuttons = document.querySelectorAll(".closeButton");
@@ -92,12 +114,20 @@ function filterCard() {
         let value = cardToRemove
           .querySelector(".filterCardSpan")
           .textContent.trim();
-        filterContent.splice(filterContent.indexOf(value), 1);
+        let index = filterContent.findIndex(
+          (item) => item.type != null && item.value == value
+        );
+
+        let some = filterContent.splice(index, 1);
+        if (some[0].type == "priceRange") {
+          clearPriceRange();
+        }
         cardToRemove.remove();
         let correspondingCheckbox = document.getElementById(value);
         if (correspondingCheckbox) {
           correspondingCheckbox.checked = false;
         }
+
         filterProducts();
       }
     });
@@ -157,7 +187,152 @@ document.getElementById("PriceHigh").addEventListener("click", () => {
   clearUI();
   updateUi("PriceHigh", products);
 });
-
+document.getElementById("SelectMin").addEventListener("click", () => {
+  let selectMin = document.getElementById("SelectMin");
+  let SelectMax = document.getElementById("SelectMax");
+  let SelectMaxValue = parseInt(document.getElementById("SelectMax").value);
+  let SelectMinValue = parseInt(selectMin.value);
+  let options = [10000, 15000, 20000, 30000, 30001];
+  let text = [];
+  options.forEach((element) => {
+    if (element > SelectMinValue) {
+      if (element == SelectMaxValue) {
+        text.push(
+          `<option value="${element}" selected>${
+            element == 30001 ? "₹30000+" : `₹${element}`
+          }</option>`
+        );
+      } else {
+        text.push(
+          `<option value="${element}" >${
+            element == 30001 ? "₹30000+" : `₹${element}`
+          }</option>`
+        );
+      }
+    }
+  });
+  SelectMax.innerHTML = text.join("");
+});
+document.getElementById("SelectMin").addEventListener("change", () => {
+  let selectMin = document.getElementById("SelectMin");
+  let SelectMax = document.getElementById("SelectMax");
+  let SelectMaxValue = parseInt(document.getElementById("SelectMax").value);
+  let SelectMinValue = parseInt(selectMin.value);
+  let options = [10000, 15000, 20000, 30000, 30001];
+  let text = [];
+  options.forEach((element) => {
+    if (element > SelectMinValue) {
+      if (element == SelectMaxValue) {
+        text.push(
+          `<option value="${element}" selected>${
+            element == 30001 ? "₹30000+" : `₹${element}`
+          }</option>`
+        );
+      } else {
+        text.push(
+          `<option value="${element}" >${
+            element == 30001 ? "₹30000+" : `₹${element}`
+          }</option>`
+        );
+      }
+    }
+  });
+  SelectMax.innerHTML = text.join("");
+  let index = filterContent.findIndex((value) => value.type == "priceRange");
+  if (index == -1) {
+    filterContent.push({
+      type: "priceRange",
+      minValue: SelectMinValue,
+      maxValue: SelectMaxValue,
+    });
+  } else {
+    filterContent.splice(index, 1, {
+      type: "priceRange",
+      minValue: SelectMinValue,
+      maxValue: SelectMaxValue,
+    });
+  }
+  filterCard();
+});
+document.getElementById("SelectMax").addEventListener("click", () => {
+  let text = [];
+  let SelectMaxValue = parseInt(document.getElementById("SelectMax").value);
+  let SelectMin = document.getElementById("SelectMin");
+  let SelectMinValue = parseInt(document.getElementById("SelectMin").value);
+  let options = [0, 10000, 15000, 20000, 30000, 30001];
+  options.forEach((element) => {
+    if (element < SelectMaxValue) {
+      if (element == SelectMinValue) {
+        if (element == 0) {
+          text.push(`<option value="${element}">Min</option>`);
+        } else {
+          text.push(
+            `<option value="${element}" selected>${
+              element == 30001 ? "₹30000+" : `₹${element}`
+            }</option>`
+          );
+        }
+      } else {
+        if (element == 0) {
+          text.push(`<option value="${element}">Min</option>`);
+        }
+        text.push(
+          `<option value="${element}">${
+            element == 30001 ? "₹30000+" : `₹${element}`
+          }</option>`
+        );
+      }
+    }
+  });
+  SelectMin.innerHTML = text.join("");
+});
+document.getElementById("SelectMax").addEventListener("change", () => {
+  let text = [];
+  let SelectMaxValue = parseInt(document.getElementById("SelectMax").value);
+  let SelectMin = document.getElementById("SelectMin");
+  let SelectMinValue = parseInt(document.getElementById("SelectMin").value);
+  let options = [0, 10000, 15000, 20000, 30000, 30001];
+  options.forEach((element) => {
+    if (element < SelectMaxValue) {
+      if (element == SelectMinValue) {
+        if (element == 0) {
+          text.push(`<option value="${element}">Min</option>`);
+        } else {
+          text.push(
+            `<option value="${element}" selected>${
+              element == 30001 ? "₹30000+" : `₹${element}`
+            }</option>`
+          );
+        }
+      } else {
+        if (element == 0) {
+          text.push(`<option value="${element}">Min</option>`);
+        }
+        text.push(
+          `<option value="${element}">${
+            element == 30001 ? "₹30000+" : `₹${element}`
+          }</option>`
+        );
+      }
+    }
+  });
+  SelectMin.innerHTML = text.join("");
+  let index = filterContent.findIndex((value) => value.type == "priceRange");
+  if (index == -1) {
+    filterContent.push({
+      type: "priceRange",
+      minValue: SelectMinValue,
+      maxValue: SelectMaxValue,
+    });
+  } else {
+    filterContent.splice(index, 1, {
+      type: "priceRange",
+      minValue: SelectMinValue,
+      maxValue: SelectMaxValue,
+    });
+  }
+  filterCard();
+});
 fetchData()
   .then((data) => {
     header = data.header;
@@ -168,6 +343,3 @@ fetchData()
   .catch((error) => {
     console.error("Error occurred:", error);
   });
-
-
-  
